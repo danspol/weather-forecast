d3f6807678b55e61262cbec5869dd3ed5fa1b7be
Dorin Turturean <dorin.turturean@gmail.com>
2018-12-14 14:46:15 -0600

    adjust visual

--- src/app/shared/weather-container/components/weather-forecast/weather-forecast.component.ts
+++ src/app/shared/weather-container/components/weather-forecast/weather-forecast.component.ts
@@ -1,5 +1,7 @@
-import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, OnChanges, SimpleChanges} from '@angular/core';
 import {IWeatherList} from '../../../services/weather-api';
+import * as moment from 'moment';
+import {reduce} from 'lodash';
 
 @Component({
   selector: 'app-weather-forecast',
@@ -7,14 +9,57 @@ import {IWeatherList} from '../../../services/weather-api';
   styleUrls: ['./weather-forecast.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class WeatherForecastComponent {
+export class WeatherForecastComponent implements OnChanges {
   @Input() forecasts: IWeatherList;
 
-  currentDay;
+  days = [];
+  groupDays = {};
+  previous;
+
   constructor() {
   }
 
+  getDay(timestamp) {
+    return moment(timestamp).format('DD');
+  }
+
+  getHour(timestamp) {
+    return moment(timestamp).format('HH:mm');
+  }
+
   isNextDay(timestamp) {
-      const day = new Date(timestamp);
+    const day = moment(timestamp).format('DD');
+
+    if (day !== this.previous) {
+      this.previous = day;
+      return true;
+    }
+
+    return false;
+  }
+
+  getForecasts(day) {
+
+    return this.groupDays[day] || [];
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+
+    if (changes.forecasts) {
+      const forecasts = changes.forecasts.currentValue;
+      this.groupDays = reduce(forecasts.list, (result, value) => {
+        const key = moment(value.dt_txt).format('DD');
+
+        if (!result[key]) {
+          result[key] = [];
+        }
+
+        result[key] = [...result[key], value];
+
+        return result;
+      }, {});
+
+      this.days = Object.keys(this.groupDays);
+    }
   }
 }
